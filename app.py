189982421005f810fc3e455c2b8bce1264a8953e
aquaholics-dv <info@aquaholics.co.uk>
2025-10-08 12:22:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ CORS(app)
 # 🔐 YOUR BÓKUN API CREDENTIALS
 # -----------------------------------------------------------------------------
 ACCESS_KEY = "75dd7122985a493ebcb1c04841ca2d17"
-SECRET_KEY = "8495ebd2d7414b8ebfd9d7253b5bdf09"
+SECRET_KEY = "00c39fd375af4b8e8888b483d14335f5"
 
 # -----------------------------------------------------------------------------
 # Product configurations
